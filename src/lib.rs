3705a480580780156ac26b579243a12a0f69f3b6
Lucas Sunsi Abreu <lsunsi@proton.me>
2026-06-25 07:54:57 -0300

    add void elements

--- src/lib.rs
+++ src/lib.rs
@@ -48,19 +48,90 @@ macro_rules! hiper {
                 + ">"
         }
     };
-    ($tag:ident {}) => {
-        |s: String| s + "<" + stringify!($tag) + "></" + stringify!($tag) + ">"
-    };
     ($tag:ident { $child:literal }) => {
         |s: String| s + "<" + stringify!($tag) + ">" + $child +"</" + stringify!($tag) + ">"
     };
     ($tag:ident { ($child:expr) }) => {
         |s: String| s + "<" + stringify!($tag) + ">" + $child +"</" + stringify!($tag) + ">"
     };
+    ($tag:ident {}) => {
+        |s: String| s + "<" + stringify!($tag) + "></" + stringify!($tag) + ">"
+    };
+    ($tag:ident $($akl:ident=$avl:literal)*, $($ake:ident=($ave:expr))*;) => {
+        |s: String| {
+            s + "<"
+                + stringify!($tag)
+                $(
+                    + " "
+                    + stringify!($akl)
+                    + "="
+                    + stringify!($avl)
+                )*
+                $(
+                    + " "
+                    + stringify!($ake)
+                    + "=\""
+                    + $ave
+                    + "\""
+                )*
+                + ">"
+        }
+    };
+    ($tag:ident $ak:ident=($av:expr);) => {
+        |s: String| {
+            s + "<"
+                + stringify!($tag)
+                + " "
+                + stringify!($ak)
+                + "=\""
+                + $av
+                + "\">"
+        }
+    };
+    ($tag:ident $ak:ident=$av:literal;) => {
+        |s: String| {
+            s + "<"
+                + stringify!($tag)
+                + " "
+                + stringify!($ak)
+                + "="
+                + stringify!($av)
+                + ">"
+        }
+    };
+    ($tag:ident;) => {
+        |s: String| s + "<" + stringify!($tag) + ">"
+    };
 }
 
 #[cfg(test)]
 mod tests {
+    #[test]
+    fn tag_void() {
+        let h = hiper! { br; }(String::new());
+        assert_eq!(&h, r#"<br>"#);
+    }
+
+    #[test]
+    fn tag_void_attr_lit() {
+        let h = hiper! { br id="berre"; }(String::new());
+        assert_eq!(&h, r#"<br id="berre">"#);
+    }
+
+    #[test]
+    fn tag_void_attr_expr() {
+        let id = "berre";
+        let h = hiper! { br id=(id); }(String::new());
+        assert_eq!(&h, r#"<br id="berre">"#);
+    }
+
+    #[test]
+    fn tag_void_attr_lit_expr() {
+        let href = "/estilo";
+        let h = hiper! { link rel="stylesheet", href=(href); }(String::new());
+        assert_eq!(&h, r#"<link rel="stylesheet" href="/estilo">"#);
+    }
+
     #[test]
     fn tag_empty() {
         let h = hiper! { a {} }(String::new());
