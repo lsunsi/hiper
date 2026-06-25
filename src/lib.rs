27d298ade7bfeb2d8a770995031c6fd0d6d637b3
Lucas Sunsi Abreu <lsunsi@proton.me>
2026-06-25 07:23:01 -0300

    add a couple cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[macro_export]
+macro_rules! hiper {
+    ($tag:ident $ak:ident=$av:literal {}) => {
+        |mut s: String| {
+            let tag = stringify!($tag);
+            s.push('<');
+            s.push_str(tag);
+            s.push(' ');
+            s.push_str(stringify!($ak));
+            s.push('=');
+            s.push_str(stringify!($av));
+            s.push_str("></");
+            s.push_str(tag);
+            s.push('>');
+            s
+        }
+    };
+    ($tag:ident {}) => {
+        |mut s: String| {
+            let tag = stringify!($tag);
+            s.push('<');
+            s.push_str(tag);
+            s.push_str("></");
+            s.push_str(tag);
+            s.push('>');
+            s
+        }
+    };
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    #[test]
+    fn tag_bare_empty() {
+        let h = hiper! { p {} }(String::new());
+        assert_eq!(&h, r#"<p></p>"#);
+    }
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn tag_empty_prop_lit() {
+        let h = hiper! { a href="/sobre" {} }(String::new());
+        assert_eq!(&h, r#"<a href="/sobre"></a>"#);
     }
 }
